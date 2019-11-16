7382d19b52348758d0f26685802a08a212209d3d
Henrik Helenius <henrik@codeberry.fi>
2019-11-16 06:07:56 +0200

    Updates

---
+++ precache-manifest.8df2134608342a72f415439c4bf2c568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d4059ac2c9b45275be676a255b05e1",
+    "url": "/laavu-2019/index.html"
+  },
+  {
+    "revision": "51f894a30d49d9bd294d",
+    "url": "/laavu-2019/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "111d1cd3c556c8e50927",
+    "url": "/laavu-2019/static/css/main.de4be9ea.chunk.css"
+  },
+  {
+    "revision": "51f894a30d49d9bd294d",
+    "url": "/laavu-2019/static/js/2.58c9a66b.chunk.js"
+  },
+  {
+    "revision": "111d1cd3c556c8e50927",
+    "url": "/laavu-2019/static/js/main.08f8be24.chunk.js"
+  },
+  {
+    "revision": "6119fc69356f31bd95d9",
+    "url": "/laavu-2019/static/js/runtime-main.c162af8c.js"
+  }
+]);
