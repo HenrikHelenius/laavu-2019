8ab3dbd6e810d5feef5d49c28d639c411d37838c
Henrik Helenius <henrik@codeberry.fi>
2019-11-17 09:31:25 +0200

    Updates

--- laavu/src/constants.js
+++
@@ -1,7 +0,0 @@
-const constants = {
-	ls_settings_id: 'laavu_settings',
-	ls_trips_id: 'laavu_trips',
-	ls_posts_id: 'laavu_posts'
-};
-
-export default constants;
---
+++ precache-manifest.6581dbd091ef6cd3b193ce366aee07b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3aad1129710ccdb4df47c1db13cd49",
+    "url": "/laavu/index.html"
+  },
+  {
+    "revision": "cb4da4bd235c52464860",
+    "url": "/laavu/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "6cdc68df5cc19ca25f0a",
+    "url": "/laavu/static/css/main.59fb3f2e.chunk.css"
+  },
+  {
+    "revision": "cb4da4bd235c52464860",
+    "url": "/laavu/static/js/2.f909d00a.chunk.js"
+  },
+  {
+    "revision": "6cdc68df5cc19ca25f0a",
+    "url": "/laavu/static/js/main.83f2f14c.chunk.js"
+  },
+  {
+    "revision": "189c8e3bfa2d0198d14e",
+    "url": "/laavu/static/js/runtime-main.168d55ba.js"
+  },
+  {
+    "revision": "53b3faee500355470aa716524e8778a5",
+    "url": "/laavu/static/media/aaro.53b3faee.jpg"
+  },
+  {
+    "revision": "8c87583ffdc369693049977e4c39f91d",
+    "url": "/laavu/static/media/laavu-svg.8c87583f.svg"
+  }
+]);
